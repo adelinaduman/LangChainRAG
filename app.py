9f81957fadd6276b4cb6fd04aca3b51a46a8fca7
adelinaduman <yelemessova.adelina@gmail.com>
2024-06-22 14:23:55 +0500

    code

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+load_dotenv()
+st.set_page_config(page_title="Knowledge Base for any Website", page_icon="💡")
+st.title("Knowledge Base for any Website")
+
+
+# Inject custom CSS for styling the app
+st.markdown("""
+<style>
+.chat-message-container {
+    padding: 10px;
+    border-radius: 15px;
+    margin-bottom: 10px;
+    box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+    font-size: 16px; /* Increase font size */
+}
+.ai-message {
+    background-color: #f0f2f6;
+}
+.human-message {
+    background-color: #daf5cb;
+}
+/* Additional styles for the app */
+.stTextInput>div>div>input {
+    font-size: 16px; /* Make input text larger */
+}
+</style>
+""", unsafe_allow_html=True)
+
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    # split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    # create a vectorstore from the chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vector_store
+
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+
+    retriever = vector_store.as_retriever()
+
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user",
+         "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return retriever_chain
+
+
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOpenAI()
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+
+    return response['answer']
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a ChatBot that can help you understand any website content. How can I help you?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)
+
+        # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+    # conversation
+    for message in st.session_state.chat_history:
+        css_class = "ai-message" if isinstance(message, AIMessage) else "human-message"
+        st.markdown(f"<div class='chat-message-container {css_class}'>{message.content}</div>", unsafe_allow_html=True)
